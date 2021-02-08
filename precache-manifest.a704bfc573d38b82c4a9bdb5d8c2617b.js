316146f67c7c3b33e8f07f20def3f72bbdebaab7
vaibhavvv9 <vaibhavgood9@gmail.com>
2021-02-08 13:28:33 +0530

    Updates

---
+++ precache-manifest.a704bfc573d38b82c4a9bdb5d8c2617b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26d9c389a6f1ae8e6ef2e3d1e363e0b",
+    "url": "/vaibhavvv9/Vaibhav_Mishra_Portfolio/index.html"
+  },
+  {
+    "revision": "32f22228e66dbdb77b42",
+    "url": "/vaibhavvv9/Vaibhav_Mishra_Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ff3a3927132869f86f68",
+    "url": "/vaibhavvv9/Vaibhav_Mishra_Portfolio/static/js/2.d0e809b4.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/vaibhavvv9/Vaibhav_Mishra_Portfolio/static/js/2.d0e809b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32f22228e66dbdb77b42",
+    "url": "/vaibhavvv9/Vaibhav_Mishra_Portfolio/static/js/main.e1cf473b.chunk.js"
+  },
+  {
+    "revision": "f6d5aee0389e184f8eab",
+    "url": "/vaibhavvv9/Vaibhav_Mishra_Portfolio/static/js/runtime-main.b70e121d.js"
+  },
+  {
+    "revision": "e5d1e75ec129abc8fb652f7f142570c8",
+    "url": "/vaibhavvv9/Vaibhav_Mishra_Portfolio/static/media/codechef.e5d1e75e.svg"
+  },
+  {
+    "revision": "f50516807239523eba78d85046093ce4",
+    "url": "/vaibhavvv9/Vaibhav_Mishra_Portfolio/static/media/codeforces.f5051680.svg"
+  }
+]);
